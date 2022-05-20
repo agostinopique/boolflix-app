553be397efa4dbeaa0c96bc9fca9c8e54979d594
agostinopique <ago.pique@yahoo.it>
2022-05-20 11:51:42 +0200

    First Commit; Layout completato;

---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,58 @@
+<template>
+    <header>
+        <div class="head-img">
+            <img src="../assets/img/Netflix-Logo.jpg" alt="">
+        </div>
+        <div class="input-area">
+            <input type="text" class="form-control input" placeholder="Oggi ho voglia di...">
+            
+            <button class="btn btn-light input-btn">Cerca</button>
+            
+            <select name="genre" id="movie-genre">
+                <option value="" selected>Select Genre</option>
+                <option value="">MOVIES OR TV</option>
+                <option value="">MOVIES OR TV</option>
+                <option value="">MOVIES OR TV</option>
+                <option value="">MOVIES OR TV</option>
+            </select>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'HeaderComp'
+}
+</script>
+
+<style lang="scss" scoped>
+header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 15px;
+    height: 100px;
+    width: 100%;
+    background-color: black;
+    .head-img{
+        width: 200px;
+        height: 100%;
+        img{
+            width: 100%;
+        }
+    }
+    .input-area{
+        width: 50%;
+        display: flex; 
+        align-items: center;
+
+        #movie-genre{
+            margin-left: 10px;
+            padding: 7px 10px;
+        }
+        .input-btn{
+            margin: 0 5px;
+        }
+    }
+}
+</style>
---
+++ src/components/MainComp.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="card-container">
+        <div class="card-element text-light">
+            <h2>titolo</h2>
+            <h2>titolo originale</h2>
+            <h2>voto</h2>
+            <h2>trama</h2>
+        </div>
+        <div class="card-element text-light">
+            <h2>titolo</h2>
+            <h2>titolo originale</h2>
+            <h2>voto</h2>
+            <h2>trama</h2>
+        </div>
+        <div class="card-element text-light">
+            <h2>titolo</h2>
+            <h2>titolo originale</h2>
+            <h2>voto</h2>
+            <h2>trama</h2>
+        </div>
+        <div class="card-element text-light">
+            <h2>titolo</h2>
+            <h2>titolo originale</h2>
+            <h2>voto</h2>
+            <h2>trama</h2>
+        </div>
+        <div class="card-element text-light">
+            <h2>titolo</h2>
+            <h2>titolo originale</h2>
+            <h2>voto</h2>
+            <h2>trama</h2>
+        </div>
+        <div class="card-element text-light">
+            <h2>titolo</h2>
+            <h2>titolo originale</h2>
+            <h2>voto</h2>
+            <h2>trama</h2>
+        </div>
+       
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MainComp'
+}
+</script>
+
+<style lang="scss" scoped>
+.card-container{
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+
+    .card-element{
+        min-width: 300px;
+        height: 600px;
+        border: 1px solid white;
+    }
+}
+</style>
